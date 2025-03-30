87853d6de41d0c83ebaa492508a5a4fe687e69b3
Jagmohan Chauhan <jagmohan.barkot@gmail.com>
2025-03-31 03:25:25 +0530

    code addition

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,7 +1,28 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.prompt import ChatPromptTemplate
-from langchain_core.output_parser import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 import streamlit as st
 import os
 from dotenv import load_dotenv
+
+# Load environment variables from .env file
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+#define the tracing mode to be used
+os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
+#used to define where all monitoring data is sent to 
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+#Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question:{question}"),
+    ]
+)
+
+## streamlit framework
+st.title("LangChain Demo with OPENAI API")
+input_text = st.text_input("Search the topic you want")
+
+## openAI LLM
